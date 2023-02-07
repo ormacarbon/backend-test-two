7d64d8833c0d042b02ab05416274bf8b20bbd471
caiooliveirah09 <leelros@hotmail.com>
2023-02-07 18:03:21 -0300

    created index.ts and make initial setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => res.send("xD"));
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
