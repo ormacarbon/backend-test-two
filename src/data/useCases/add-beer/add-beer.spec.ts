ddff37276aa4e91c871078949d6fb01fbdf04edf
Douglinhas <daguiaralcantara@gmail.com>
2023-02-07 14:26:25 -0300

    git commit -m "test: ensure DbAddBeer work right"

---
+++ src/data/useCases/add-beer/add-beer.spec.ts
@@ -0,0 +1,72 @@
+import { BeerModels } from "../../../domain/models/beer";
+import { AddBeerData } from "../../../domain/useCases/add-beer";
+import { AddBeerRepository } from '../../protocols/add-beer-repository';
+import { DbAddBeer } from "./add-beer";
+
+const fakeBeerData = {
+  name: "Stone House Stout",
+  abv: 8.918797384901016,
+  address:"141 South Main Street",
+  category:"British Ale",
+  city:"Slippery Rock",
+  coordinates:[41.0638,-80.0556],
+  country:"United States",
+  description:"This robust, hearty stout is as sturdy as its namesake.  Roasted barley is the trademark of stout, a bittersweet separation from its cousin Porter.  The deep character of roasted barley is further enhanced by the addition of oatmeal for an incredible silky finish.",
+  ibu:104,
+  state:"Pennsylvania",
+  website:"http://www.northcountrybrewing.com"
+}
+
+const makeAddBeerRepository = (): AddBeerRepository => {
+  class AddBeerRepositoryStub implements AddBeerRepository {
+    add(beerData: AddBeerData): Promise<BeerModels> {
+      const fakeBeer = {
+        id: 1,
+        ...fakeBeerData
+      }
+
+      return new Promise(resolve => resolve(fakeBeer));
+    }
+  }
+
+  return new AddBeerRepositoryStub();
+}
+
+interface SutTypes {
+  sut: DbAddBeer
+  addBeerRepositoryStub: AddBeerRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addBeerRepositoryStub = makeAddBeerRepository();
+  const sut = new DbAddBeer(addBeerRepositoryStub);
+
+  return {
+    sut,
+    addBeerRepositoryStub
+  }
+}
+
+describe("AddBeer", () => {
+  it("Should throw if AddBeerRepository throws", async () => {
+    const { sut, addBeerRepositoryStub } = makeSut();
+
+    jest.spyOn(addBeerRepositoryStub, "add").mockImplementationOnce(() => {
+      throw new Error();
+    })
+    const promise = sut.add(fakeBeerData);
+
+    expect(promise).rejects.toThrow();
+  })
+
+  it("Should return the correct values on success", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.add(fakeBeerData);
+
+    expect(response).toEqual({
+      id: 1,
+      ...fakeBeerData
+    });
+  })
+})
