9d096710129da0b78f050e59b35a4b2006b7d94a
vitinhos67 <santtosvictor42@gmail.com>
2023-02-08 12:31:03 -0300

    docs(document file readFileJSON): documenting the funtions

--- src/common/utils/readFileJSON.ts
+++ src/common/utils/readFileJSON.ts
@@ -1,13 +1,30 @@
 import fs from 'fs';
 import Path = require('path');
 
+/**
+ *
+ * @param path Path from file recevied.
+ * @summary Unlink the file recevied through of param.
+ */
+
 export function unlinkFile(path: string): void {
   fs.unlinkSync(path);
 }
 
+/**
+ * @param path Path from file recevied.
+ * @summary Read the file recevied through of param.
+ */
+
 function readFile(path: string): string | void {
   return fs.readFileSync(path, { encoding: 'utf-8' });
 }
+
+/**
+ *
+ * @param path Path from file recevied.
+ * @summary Read the file and return its data, before, respectively, calling the readFile and unlinkFile function.
+ */
 
 export async function catchContent(path: string): Promise<string | void> {
   path = Path.resolve(__dirname, '..', '..', '..', path);
