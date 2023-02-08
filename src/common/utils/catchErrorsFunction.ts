e0a306c8ee3b0c3c9c93fa42a7e29aa5174f0410
vitinhos67 <santtosvictor42@gmail.com>
2023-02-08 10:57:52 -0300

    fix: check integrity the file JSON in endpoint

--- src/common/utils/catchErrorsFunction.ts
+++ src/common/utils/catchErrorsFunction.ts
@@ -4,11 +4,18 @@ import {
 } from '../../services/err/Errors';
 
 export default function cacthErrosFunctions<T>(error: T): void {
+  if (error instanceof SyntaxError) {
+    console.log(error.message);
+
+    if (error.message.includes('JSON')) {
+      throw new InvalidArgumentError(`Verify integrations of your file.`);
+    }
+  }
+
   if (error instanceof InvalidArgumentError) {
     throw new InvalidArgumentError(error.message);
   }
 
-  console.log(error);
   if (error) {
     throw new InternalServerError(error as string);
   }
