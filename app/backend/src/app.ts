0e7e243fea01894d32e9720ea35a5046a85209fe
Airam Toscano <airamlobato@gmail.com>
2023-02-08 14:54:23 -0400

    feat: install helmet

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import 'express-async-errors';
+import helmet from 'helmet';
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
 app.use(cors());
 
 export default app;
