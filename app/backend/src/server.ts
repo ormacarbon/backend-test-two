5c0462b8b87f20c897b960b3f0c6853822b23b0f
Airam Toscano <airamlobato@gmail.com>
2023-02-08 11:22:39 -0400

    fix: eslint and github actions

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,13 +1,14 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
