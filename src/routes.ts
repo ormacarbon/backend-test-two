87885d3eb6a8ddfea57383f2feaef8f8f01b5e63
K0dax <yancarlosbrg@outlook.com>
2023-02-08 15:45:25 -0300

    feat(routes): clients filtered by query

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateClientController } from "./controllers/CreateClientController";
 import { DetailsClientController } from "./controllers/DetailsClientController";
 import { AllClientsController } from "./controllers/AllClientsController.ts";
+import { GetFilteredClientsController } from "./controllers/GetFilteredClientsController";
 import { UpdateClientController } from "./controllers/UpdateClientController";
 import { DeleteClientController } from "./controllers/DeleteClientController";
 
@@ -13,6 +14,8 @@ router.post("/client", new CreateClientController().handle);
 router.get("/detail", new DetailsClientController().handle);
 
 router.get("/clients", new AllClientsController().handle);
+
+router.get("/filtered", new GetFilteredClientsController().handle);
 
 router.put("/update", new UpdateClientController().handle);
 
