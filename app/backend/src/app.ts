19f9d8e2e579042927e848dbc0924ac24b61a855
Airam Toscano <airamlobato@gmail.com>
2023-02-08 14:58:11 -0400

    feat: create rateLimit for api

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 50, // limit each IP to 50 requests per windowMs
+  message: 'Too many accounts created from this IP, please try again after a minute',
+});
 
 const app = express();
 app.use(express.json());
+app.use(limiter);
 app.use(helmet());
 app.use(cors());
 
