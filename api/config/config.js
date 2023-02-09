1b6cde19a4841029eae11a1760081691cded7942
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2023-02-09 15:55:57 -0300

    FTApi-M: Added start script for test deployment on the railway

--- api/config/config.js
+++ api/config/config.js
@@ -15,10 +15,11 @@ module.exports = {
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASS,
+      "database": process.env.DB_NAME,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT,
+      "port": process.env.DB_PORT
     }
   }
