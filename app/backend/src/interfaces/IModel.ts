765644a41eeed93be5919d8f6bf536ab679e320b
Airam Toscano <airamlobato@gmail.com>
2023-02-09 10:53:04 -0400

    feat: implement update interface model

--- app/backend/src/interfaces/IModel.ts
+++ app/backend/src/interfaces/IModel.ts
@@ -1,5 +1,6 @@
 export interface IModel <T> {
   create(obj:T):Promise<T>,
   readBeer(name: string):Promise<T>,
-  readAll():Promise<T[]>
+  readAll():Promise<T[]>,
+  update(id:string, obj:Partial<T>):Promise<T | null>,
 }
