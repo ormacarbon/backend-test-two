36e7e0add1a08bffb6445f468270170cdd278997
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-09 08:36:17 -0300

    feature: add brewery domain

---
+++ src/domain/entities/brewery.ts
@@ -0,0 +1,13 @@
+export type Brewery = {
+  abv: number
+  adress?: string
+  category?: string
+  city?: string
+  coordinates?: number[]
+  country?: string
+  description?: string
+  ibu: number
+  name?: string
+  state?: string
+  website?: string
+}
