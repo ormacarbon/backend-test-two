3627f480c4501d6d93c25f2ca90f2cae91d4ba80
gabriel <gabriel.pr07@hotmail.com>
2023-02-09 14:13:27 -0300

    feat: implement UpdateBeerByIdController

---
+++ backend/src/presentation/controllers/beer/update-beer-by-id/update-beer-by-id-controller.ts
@@ -0,0 +1,28 @@
+import { UpdateBeerById } from '../../../../domain/use-cases/update-beer-by-id'
+import { noContent, serverError } from '../../../helpers/http/http-helper'
+import { Controller } from '../../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../../protocols/http'
+
+export class UpdateBeerByIdController implements Controller {
+	private readonly updateBeer: UpdateBeerById
+
+	constructor (updateBeer: UpdateBeerById) {
+		this.updateBeer = updateBeer
+	}
+
+	async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const {
+				id
+			} = httpRequest.params
+
+			const beerCreateRequest = httpRequest.body
+
+			await this.updateBeer.update(id, beerCreateRequest)
+
+			return noContent()
+		} catch (err: any) {
+			return serverError(err)
+		}
+	}
+}
