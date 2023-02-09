5561e8a1fe46fb6c1999dc1f7837f3626aa93c0a
pabloghid <pablodg290@gmail.com>
2023-02-09 17:21:28 -0300

    added swagger for docs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import beers from "./beersRoutes.js";
+import swaggerUi from "swagger-ui-express"
+// to import JSON file
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const swaggerDocs = require("../src/docs/swagger.json");
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
         res.status(200).send("Beer api")
     })
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)) 
 
     app.use(
         express.json(), beers
