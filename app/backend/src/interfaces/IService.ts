254614874c537080d93df129ea86c6fe806a9b27
Airam Toscano <airamlobato@gmail.com>
2023-02-09 11:14:37 -0400

    feat: implement delete interfaces Iservice

--- app/backend/src/interfaces/IService.ts
+++ app/backend/src/interfaces/IService.ts
@@ -2,6 +2,7 @@ interface IBeersService<T> {
   create(obj: T): Promise<T | Error>,
   readAll():Promise<T[]>,
   update(_id:string, obj:Partial<T>):Promise<T>,
+  delete(_id:string):Promise<T>,
 }
   
 export default IBeersService;
