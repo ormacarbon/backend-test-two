259c8fd536493e3ccfd073bf4f6d2eef53325aea
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-10 10:24:40 -0300

    test: should throw if LoadBreweriesRepository throws

--- tests/data/usecases/db-load-breweries.spec.ts
+++ tests/data/usecases/db-load-breweries.spec.ts
@@ -1,4 +1,5 @@
 import { DbLoadBreweries } from '../../../src/data/usecases/db-load-breweries'
+import { throwError } from '../../domain/mocks/test-helpers'
 import { LoadBreweriesRepositorySpy } from '../mocks/mock-db-brewery'
 
 type SutTypes = {
@@ -17,5 +18,12 @@ describe('DbLoadBreweries Usecase', () => {
     const { sut, loadBreweriesRepositorySpy } = makeSut()
     await sut.handle()
     expect(loadBreweriesRepositorySpy.call).toBe(true)
+  })
+
+  it('Should throw if LoadBreweriesRepository throws', async () => {
+    const { sut, loadBreweriesRepositorySpy } = makeSut()
+    jest.spyOn(loadBreweriesRepositorySpy, 'handle').mockImplementationOnce(throwError)
+    const result = sut.handle()
+    await expect(result).rejects.toThrow()
   })
 })
