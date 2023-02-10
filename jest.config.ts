63a9989c7ba1e717b84216951447c39b73a8dd3d
Mike Valeriano <mkvlrn@gmail.com>
2023-02-10 04:38:53 -0300

    chore: setup jest, lintstaged, commitlint

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^#/(.*)\\.jsx?$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+.(png|svg|jpg|gif|webp)$': 'jest-transform-stub',
+  },
+  passWithNoTests: true,
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  testRegex: '.(spec|test).(ts|tsx)$',
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageProvider: 'v8',
+  setupFiles: ['<rootDir>/jest.setup.ts'],
+};
+
+export default config;
