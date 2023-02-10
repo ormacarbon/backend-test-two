5048058627ecc8276852a28eab54f74349e3a719
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-10 10:17:18 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/db-load-breweries.spec.ts
+++ tests/data/usecases/db-load-breweries.spec.ts
@@ -1,10 +1,20 @@
 import { DbLoadBreweries } from '../../../src/data/usecases/db-load-breweries'
 import { LoadBreweriesRepositorySpy } from '../mocks/mock-db-brewery'
 
+type SutTypes = {
+  loadBreweriesRepositorySpy: LoadBreweriesRepositorySpy
+  sut: DbLoadBreweries
+}
+
+const makeSut = (): SutTypes => {
+  const loadBreweriesRepositorySpy = new LoadBreweriesRepositorySpy()
+  const sut = new DbLoadBreweries(loadBreweriesRepositorySpy)
+  return { sut, loadBreweriesRepositorySpy }
+}
+
 describe('DbLoadBreweries Usecase', () => {
   it('Should call LoadBreweriesRepository', async () => {
-    const loadBreweriesRepositorySpy = new LoadBreweriesRepositorySpy()
-    const sut = new DbLoadBreweries(loadBreweriesRepositorySpy)
+    const { sut, loadBreweriesRepositorySpy } = makeSut()
     await sut.handle()
     expect(loadBreweriesRepositorySpy.call).toBe(true)
   })
