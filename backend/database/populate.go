2b1716d7e4cf37fc9160f8de32cc91932fef7449
Gabriel Morelli <gabriel.morelli@codenapp.com>
2023-02-10 20:43:51 -0300

    Populate database

--- backend/database/populate.go
+++ backend/database/populate.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ronanzindev/backend-test-two/models"
+)
+
+func PopulateDB() {
+	db := GetDataBase()
+	var beers []models.Beer
+	err := db.Find(&beers).Error
+	if err != nil {
+		fmt.Println(err)
+	}
+	if len(beers) == 0 {
+		byteValue, _ := os.ReadFile("db.json")
+		dec := json.NewDecoder(bytes.NewReader(byteValue))
+		dec.Decode(&beers)
+		err = db.CreateInBatches(&beers, 1000).Error
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+}
