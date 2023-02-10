32ab1146cd85d4ef290c3a06f8f90bd18c819ee5
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-10 17:31:32 -0300

    test: ensure return breweries on success

---
+++ tests/infra/db/prisma/load-breweries-prisma-repository.spec.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+import { LoadBreweriesPrismaRepository } from '../../../../src/infra/db/prisma/load-breweries-prisma-repository'
+import { PrismaHelper } from '../../../../src/infra/db/prisma/prisma-helper'
+import { mockBreweryEntity } from '../../../domain/mocks/mock-brewery'
+
+const makeSut = () => {
+  return new LoadBreweriesPrismaRepository()
+}
+
+const prisma = new PrismaClient()
+
+describe('LoadBreweriesPrismaRepository', () => {
+  beforeAll(async () => {
+    await PrismaHelper.connect()
+  })
+
+  afterAll(async () => {
+    await PrismaHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await prisma.brewery.deleteMany()
+  })
+
+  it('Should return breweries on success', async () => {
+    const sut = makeSut()
+    await prisma.brewery.create({ data: mockBreweryEntity() })
+    const result = await sut.handle()
+    expect(result[0].abv).toBeTruthy()
+  })
+})
