287a11b1f6774683d40a28604e2f86ab8f23d3a6
caiooliveirah09 <leelros@hotmail.com>
2023-02-10 19:46:42 -0300

    create Beer domain and IBeer interface

---
+++ src/domains/Beer.ts
@@ -0,0 +1,43 @@
+import IBeer from "../interfaces/IBeer";
+
+export default class Beer {
+  id?: string;
+  abv: number;
+  address: string;
+  category: string;
+  city: string;
+  coordinates: number[];
+  country: string;
+  description: string;
+  ibu: number;
+  name: string;
+  state: string;
+  website: string;
+  constructor({
+    id,
+    abv,
+    address,
+    category,
+    city,
+    coordinates,
+    country,
+    description,
+    ibu,
+    name,
+    state,
+    website,
+  }: IBeer) {
+    this.id = id;
+    this.abv = abv;
+    this.address = address;
+    this.category = category;
+    this.city = city;
+    this.coordinates = coordinates;
+    this.country = country;
+    this.description = description;
+    this.ibu = ibu;
+    this.name = name;
+    this.state = state;
+    this.website = website;
+  }
+}
