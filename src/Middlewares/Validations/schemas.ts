4cebf989cf54c72f7dc6e7e6bbd863c433daae08
Hugo <huleonardo19@gmail.com>
2023-02-10 18:02:04 -0300

    refactor: atualiza valores máximo de alguns schemas.

--- src/Middlewares/Validations/schemas.ts
+++ src/Middlewares/Validations/schemas.ts
@@ -4,20 +4,20 @@ const abvSchema = Joi.number().empty().min(1).required();
 
 const addressSchema = Joi.string().empty().min(3).max(100);
 
-const categorySchema = Joi.string().empty().min(3).max(15);
+const categorySchema = Joi.string().empty().min(3).max(50);
 
-const citySchema = Joi.string().empty().min(3).max(15);
+const citySchema = Joi.string().empty().min(3).max(50);
 
-const countrySchema = Joi.string().empty().min(3).max(15);
+const countrySchema = Joi.string().empty().min(3).max(50);
 
 const coordinatesSchema = Joi.array().items(Joi.number().min(1).required());
 
 const ibuSchema = Joi.number().empty().min(1).required();
 
-const nameSchema = Joi.string().empty().min(3).max(15)
+const nameSchema = Joi.string().empty().min(3).max(50)
   .required();
 
-const stateSchema = Joi.string().empty().min(3).max(15);
+const stateSchema = Joi.string().empty().min(3).max(50);
 
 const websiteSchema = Joi.string().empty().min(3).max(50);
 
