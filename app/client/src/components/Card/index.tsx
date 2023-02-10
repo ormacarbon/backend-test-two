ce1b63bf727105016ab1e0e29b705e48d3f534f1
Debora Fantini <deboraferfan@gmail.com>
2023-02-10 16:39:13 -0300

    feat: list all beers home page

--- app/client/src/components/Card/index.tsx
+++ app/client/src/components/Card/index.tsx
@@ -1,13 +1,22 @@
-function Card() {
+import { IBeer } from "../../interfaces/IBeer";
+
+type Props = {
+  beer: IBeer
+}
+
+function Card({beer}: Props) {
   return (
     <div>
-      <p><strong>Title:</strong> Describe</p>
-      <p><strong>Title:</strong> Describe</p>
-      <p><strong>Title:</strong> Describe</p>
-      <p><strong>Title:</strong> Describe</p>
-      <p><strong>Title:</strong> Describe</p>
-      <p><strong>Title:</strong> Describe</p>
-      <button>MORE...</button>
+      {beer.abv ? <p><strong>ABV:</strong>{beer.abv}</p> : null}
+      {beer.address ? <p><strong>Address:</strong>{beer.address}</p> : null}
+      {beer.category ? <p><strong>Category:</strong>{beer.category}</p> : null}
+      {beer.city ? <p><strong>City:</strong>{beer.city}</p> : null}
+      {beer.coordinates ? <p><strong>Coordinates:</strong>{beer.coordinates}</p> : null}
+      {beer.description ? <p><strong>Description:</strong>{beer.description}</p> : null}
+      {beer.ibu ? <p><strong>IBU:</strong>{beer.ibu}</p> : null}
+      {beer.name ? <p><strong>Name:</strong>{beer.name}</p> : null}
+      {beer.state ? <p><strong>State:</strong>{beer.state}</p> : null}
+      {beer.website ? <p><strong>Website:</strong>{beer.website}</p> : null}
     </div>
   );
 }
--- app/client/src/pages/Home/index.tsx
+++ app/client/src/pages/Home/index.tsx
@@ -1,14 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import Header from "../../components/Header";
+import { IBeer } from "../../interfaces/IBeer";
 
 function Home() {
+  const [beers, setBeers] = useState<IBeer[]>([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3005/beers/').then(data => {
+      setBeers(data.data)
+    }) as unknown as IBeer[];
+  }, [beers])
+
   return (
     <div>
       <Header/>
       <main>
-        <Card/>
-        <Card/>
-        <Card/>
+        {
+          beers.length > 0 ? (beers.map((beer) => (
+            <Card key={beer.id} beer={beer}/>
+          ))) : null
+        }
       </main>
     </div>
   );
