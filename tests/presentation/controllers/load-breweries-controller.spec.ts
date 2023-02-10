76ad5e46b577e11a0245c0f23f0eb4f9599903ca
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-09 21:21:58 -0300

    test: ensure return 500 if LoadBreweries thorws

--- tests/presentation/controllers/load-breweries-controller.spec.ts
+++ tests/presentation/controllers/load-breweries-controller.spec.ts
@@ -1,4 +1,6 @@
 import { LoadBreweriesController } from '../../../src/presentation/controllers/load-breweries-controller'
+import { serverError } from '../../../src/presentation/helpers/http-helper'
+import { throwError } from '../../domain/mocks/test-helpers'
 import { LoadBreweriesSpy } from '../mocks/mock-brewery'
 
 type SutTypes = {
@@ -17,5 +19,12 @@ describe('LoadBreweries Controller', () => {
     const { sut, loadBreweriesSpy } = makeSut()
     await sut.handle()
     expect(loadBreweriesSpy.call).toBe(true)
+  })
+
+  it('Should return 500 if LoadBreweries throws', async () => {
+    const { sut, loadBreweriesSpy } = makeSut()
+    jest.spyOn(loadBreweriesSpy, 'handle').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
