93081fa65d53d36c226a25141be8034ed6504aac
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-09 21:21:38 -0300

    feat: ensure return 500 if LoadBreweries thorws

--- src/presentation/controllers/load-breweries-controller.ts
+++ src/presentation/controllers/load-breweries-controller.ts
@@ -1,14 +1,16 @@
 import { LoadBreweries } from '../../domain/usecases/load-breweries'
 import { Controller } from '../abstract/controller'
 import { HttpResponse } from '../abstract/http'
+import { ok, serverError } from '../helpers/http-helper'
 
 export class LoadBreweriesController implements Controller {
   constructor (private readonly loadBreweries: LoadBreweries) {}
   async handle (): Promise<HttpResponse> {
-    await this.loadBreweries.handle()
-    return {
-      statusCode: 200,
-      body: {}
+    try {
+      await this.loadBreweries.handle()
+      return ok({})
+    } catch (error: any) {
+      return serverError(error)
     }
   }
 }
---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,12 @@
+import { HttpResponse } from '../abstract/http'
+import { ServerError } from '../errors/server-error'
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack ?? 'ServerError')
+})
