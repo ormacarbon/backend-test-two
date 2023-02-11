7d7e1b3b4f7c841ad49516ff8c8047a2d7840b00
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-11 11:35:01 -0300

    feat: ensure return 204 on success

--- src/presentation/controllers/delete-brewery-controller.ts
+++ src/presentation/controllers/delete-brewery-controller.ts
@@ -1,13 +1,13 @@
 import { DeleteBrewery } from '../../domain/usecases/delete-brewery'
 import { Controller } from '../abstract/controller'
 import { HttpResponse } from '../abstract/http'
-import { ok } from '../helpers/http-helper'
+import { noContent } from '../helpers/http-helper'
 
 export class DeleteBreweryController implements Controller {
   constructor (private readonly deleteBrewery: DeleteBrewery) {}
   async handle (request: DeleteBreweryControllerRequest): Promise<HttpResponse> {
     await this.deleteBrewery.handle(request)
-    return ok({})
+    return noContent()
   }
 }
 
