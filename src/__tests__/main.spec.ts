9b134c1b220cca3e539016dd89a451cb7271ae75
ZauJulio <zaujulio.dev@gmail.com>
2023-02-11 10:14:27 -0300

    test(e2e): add app module tests

--- src/__tests__/main.spec.ts
+++ src/__tests__/main.spec.ts
@@ -2,6 +2,8 @@ import { default as request } from 'supertest';
 
 import { bootstrap } from '../main';
 
+jest.useFakeTimers();
+
 describe('App', () => {
   const app = bootstrap().app;
 
@@ -9,19 +11,7 @@ describe('App', () => {
     return await request(app).get('/').expect(200);
   });
 
-  it('should have a /beer endpoint', (done) => {
-    request(app).post('/beer').expect(400, done);
-  });
-
   it('should have a /docs endpoint', async () => {
     await request(app).patch('/docs').expect(200);
-  });
-
-  it('should be send hello', async () => {
-    const res = await request(app).post('/beer').send({
-      name: 'Neith',
-    });
-
-    expect(res.body).toEqual({ message: 'Hello World, Neith' });
   });
 });
