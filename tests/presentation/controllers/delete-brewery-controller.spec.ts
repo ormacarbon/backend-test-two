b69585eedab8c8837583f8afe1b6f99bdeebbed3
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-11 11:40:09 -0300

    test: ensure return 500 if DeleteBrewery throws

--- tests/presentation/controllers/delete-brewery-controller.spec.ts
+++ tests/presentation/controllers/delete-brewery-controller.spec.ts
@@ -1,7 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { DeleteBreweryController, DeleteBreweryControllerRequest } from '../../../src/presentation/controllers/delete-brewery-controller'
 import { DeleteBrewerySpy } from '../mocks/mock-brewery'
-import { noContent } from '../../../src/presentation/helpers/http-helper'
+import { noContent, serverError } from '../../../src/presentation/helpers/http-helper'
+import { throwError } from '../../domain/mocks/test-helpers'
 
 const mockRequest = (): DeleteBreweryControllerRequest => ({
   id: faker.datatype.uuid()
@@ -30,5 +31,12 @@ describe('DeleteBrewery Controller', () => {
     const { sut } = makeSut()
     const result = await sut.handle(mockRequest())
     expect(result).toEqual(noContent())
+  })
+
+  it('Should return 500 if DeleteBrewery throws', async () => {
+    const { sut, deleteBrewerySpy } = makeSut()
+    jest.spyOn(deleteBrewerySpy, 'handle').mockImplementationOnce(throwError)
+    const result = await sut.handle(mockRequest())
+    expect(result).toEqual(serverError(new Error()))
   })
 })
