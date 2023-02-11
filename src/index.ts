772fd4e0ad1cc3951f85b9df1a74b30baf700cad
Mike Valeriano <mkvlrn@gmail.com>
2023-02-11 07:13:17 -0300

    fix: add seed service to container register

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { BeerController } from '#/modules/beer/beer.controller.js';
 import { BeerRouter } from '#/modules/beer/beer.router.js';
 import { BeerService } from '#/modules/beer/beer.service.js';
 import { EnvService } from '#/modules/shared/env.service.js';
+import { SeedService } from '#/modules/shared/seed.service.js';
 
 // middlewares
 container.register('IOpenApiMiddleware', { useClass: OpenApiMiddleware });
@@ -22,6 +23,7 @@ container.register('IPrismaService', { useValue: new PrismaClient() });
 
 // modules
 container.register('IBeerService', { useClass: BeerService });
+container.register('ISeedService', { useClass: SeedService });
 container.register('IBeerController', { useClass: BeerController });
 container.register('IBeerRouter', { useClass: BeerRouter });
 
