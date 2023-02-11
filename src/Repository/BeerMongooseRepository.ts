fe3602a8f717166aaa79277db64fde578a6d00a8
Hugo <huleonardo19@gmail.com>
2023-02-11 00:09:33 -0300

    refactor(BeerMongooseRepository): altera método create e remove beerDTO.

--- src/Repository/BeerMongooseRepository.ts
+++ src/Repository/BeerMongooseRepository.ts
@@ -1,26 +1,12 @@
 import BeersODM from '../Database/Models/BeersODM';
 import IBeerRepository from '../Interfaces/IBeerRepository';
-import Beer from '../Domain/Beer';
 import IBeer from '../Interfaces/IBeer';
 
-class BeerMongooseRepository implements IBeerRepository<Beer, IBeer> {
+class BeerMongooseRepository implements IBeerRepository<IBeer> {
   constructor(private _persistence = new BeersODM()) {}
 
-  public async create(beer: Beer): Promise<IBeer> {
-    const beerDTO: IBeer = {
-      abv: beer.abv,
-      address: beer.address,
-      category: beer.category,
-      city: beer.city,
-      coordinates: beer.coordinates,
-      country: beer.country,
-      ibu: beer.ibu,
-      name: beer.name,
-      description: beer.description,
-      state: beer.state,
-      website: beer.website,
-    };
-    return this._persistence.create(beerDTO);
+  public async create(beer: IBeer): Promise<IBeer> {
+    return this._persistence.create(beer);
   }
 
   public async readAll(): Promise<IBeer[]> {
