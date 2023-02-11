9210811ed4c6032aac34d39ed5ccf84669c6a71b
Gabriel Morelli <gabriel.morelli@codenapp.com>
2023-02-10 23:27:28 -0300

    Front: Create form page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Create from './pages/create';
 
 function App() {
   return (
     <div className='main'>
     <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/create' element={<Create />} />
       </Routes>
     </div>
   );
