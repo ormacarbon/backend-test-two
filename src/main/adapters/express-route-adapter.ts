1fc49714341d61f21bbfbdae7780164524a28ffc
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-11 10:02:14 -0300

    feat: add express router adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../presentation/abstract/controller'
+
+export const adaptRoute = (Controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {})
+    }
+    const httpResponse = await Controller.handle(request)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json(httpResponse.body.message)
+    }
+  }
+}
