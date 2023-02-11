e73e20d779f3ff468b33b908548aaf782ae8e228
ZauJulio <zaujulio.dev@gmail.com>
2023-02-11 10:03:48 -0300

    fix(tests): preserver NODE_ENV prop on dontenv load

--- src/common/config/env.ts
+++ src/common/config/env.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 
-const env = dotenv.config();
+const env = dotenv.config({ override: false });
 
 if (env.error) throw new Error(`⚠️  Couldn't find .env file  ⚠️`);
 
-process.env = { ...process.env, ...env.parsed };
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { NODE_ENV, ...rest } = env.parsed ?? {};
+
+process.env = { ...process.env, ...rest };
 
 export { env };
