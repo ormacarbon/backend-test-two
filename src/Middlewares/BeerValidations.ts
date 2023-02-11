e55d98d1a5612e189aa77551135d4e97571d869d
Hugo <huleonardo19@gmail.com>
2023-02-11 00:33:45 -0300

    feat(BeerValidations): cria método de validação checkPropertiesToUpdate.
    Além disso houve uma pequena refatoração no nome do objeto vindo através de req.body.

--- src/Middlewares/BeerValidations.ts
+++ src/Middlewares/BeerValidations.ts
@@ -5,12 +5,12 @@ import ValidationsInputs from './Validations/ValidationInputs';
 
 class BeerValidations {
   public static propertiesToCreate(req: Request, _res: Response, next: NextFunction) {
-    const beer = req.body;
+    const beerPropeties = req.body;
     const properties = ['abv', 'ibu', 'name'];
 
     try {
       properties.forEach((property) => {
-        const checkProperty = property in beer;
+        const checkProperty = property in beerPropeties;
         if (!checkProperty) throw new BadRequestError(`${property} attribute is missing.`);
       });
 
@@ -21,14 +21,32 @@ class BeerValidations {
   }
 
   public static checkProperties(req: Request, _res: Response, next: NextFunction) {
-    const beer = req.body;
+    const beerPropeties = req.body;
     try {
-      const { isError, message } = ValidationsInputs.validateBeerObject(beer);
+      const { isError, message } = ValidationsInputs.validateBeerObject(beerPropeties);
       if (!isError) return next();
 
       throw new UnprocessableEntityError(message);
     } catch (error) {
       return next(error);
+    }
+  }
+
+  public static checkPropertiesToUpdate(req: Request, _res: Response, next: NextFunction) {
+    const beerPropeties = req.body;
+    try {
+      const properties = [
+        'abv', 'address', 'category', 'city', 'coordinates',
+        'country', 'ibu', 'name', 'description', 'state', 'website'];
+
+      const checkProperties = properties.some((property) => property in beerPropeties);
+      if (!checkProperties || !beerPropeties) {
+        throw new BadRequestError('Missing property to update a document.');
+      }
+
+      return next();
+    } catch (error) {
+      next(error);
     }
   }
 }
