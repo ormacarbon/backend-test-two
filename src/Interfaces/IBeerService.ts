895788fe824404a105f116beb0312d6f7003c859
Hugo <huleonardo19@gmail.com>
2023-02-11 00:58:31 -0300

    feat(IBeerService): adiciona método delete a interface.

--- src/Interfaces/IBeerService.ts
+++ src/Interfaces/IBeerService.ts
@@ -1,5 +1,10 @@
+type deleteReturn = {
+  deleted: boolean,
+};
+
 export default interface IBeerService<T>{
   create(beer: T): Promise<T>;
   readAll(): Promise<T[]>;
   update(id: string, beer: Partial<T>): Promise<T | null>;
+  delete(id: string): Promise<deleteReturn>
 }
