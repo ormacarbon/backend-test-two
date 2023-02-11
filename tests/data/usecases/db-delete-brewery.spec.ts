b4de95a2fde28fbb5ee4080238b467ac2238d45f
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-11 14:09:36 -0300

    refactor: move sut creation for factory method

--- tests/data/usecases/db-delete-brewery.spec.ts
+++ tests/data/usecases/db-delete-brewery.spec.ts
@@ -2,10 +2,20 @@ import { DbDeleteBrewery } from '../../../src/data/usecases/db-delete-brewery'
 import { mockDeleteBreweryParams } from '../../domain/mocks/mock-brewery'
 import { DeleteBreweryRepositorySpy } from '../mocks/mock-db-brewery'
 
+type SutTypes = {
+  sut: DbDeleteBrewery
+  deleteBreweryRepositorySpy: DeleteBreweryRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const deleteBreweryRepositorySpy = new DeleteBreweryRepositorySpy()
+  const sut = new DbDeleteBrewery(deleteBreweryRepositorySpy)
+  return { deleteBreweryRepositorySpy, sut }
+}
+
 describe('DbDeleteBrewery Usecase', () => {
   it('Should call DeleteBreweryRepository with correct values', async () => {
-    const deleteBreweryRepositorySpy = new DeleteBreweryRepositorySpy()
-    const sut = new DbDeleteBrewery(deleteBreweryRepositorySpy)
+    const { sut, deleteBreweryRepositorySpy } = makeSut()
     const params = mockDeleteBreweryParams()
     await sut.handle(params)
     expect(deleteBreweryRepositorySpy.params).toBe(params)
