71ec26cffcbf4f71ef35090a399deb19601ecde3
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-11 11:33:05 -0300

    refactor: move sut creation for factory method

--- tests/presentation/controllers/delete-brewery-controller.spec.ts
+++ tests/presentation/controllers/delete-brewery-controller.spec.ts
@@ -6,10 +6,20 @@ const mockRequest = (): DeleteBreweryControllerRequest => ({
   id: faker.datatype.uuid()
 })
 
+type SutTypes = {
+  deleteBrewerySpy: DeleteBrewerySpy
+  sut: DeleteBreweryController
+}
+
+const makeSut = (): SutTypes => {
+  const deleteBrewerySpy = new DeleteBrewerySpy()
+  const sut = new DeleteBreweryController(deleteBrewerySpy)
+  return { deleteBrewerySpy, sut }
+}
+
 describe('DeleteBrewery Controller', () => {
   it('Should call DeleteBrewery with correct values', async () => {
-    const deleteBrewerySpy = new DeleteBrewerySpy()
-    const sut = new DeleteBreweryController(deleteBrewerySpy)
+    const { sut, deleteBrewerySpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(deleteBrewerySpy.params).toBe(request)
