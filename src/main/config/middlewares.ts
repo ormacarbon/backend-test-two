0b53f8e034d41cab5a69628c3c794b911fd56689
Douglinhas <daguiaralcantara@gmail.com>
2023-02-11 23:22:45 -0300

    feat: create cors middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
-import { Express } from "express"
-import { bodyParser } from "../middlewares/body-parser"
+import { Express } from "express";
+import { bodyParser, cors } from "../middlewares";
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 }
