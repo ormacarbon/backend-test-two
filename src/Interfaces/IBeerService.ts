5b1daf5c91fd9e559b5f2d12e2b3c448a73d7aea
Hugo <huleonardo19@gmail.com>
2023-02-12 16:50:17 -0300

    feat: cria método readById nas interfaces.
    Interfaces que recebem o método: IBeerRepository e IBeerService.

--- src/Interfaces/IBeerService.ts
+++ src/Interfaces/IBeerService.ts
@@ -5,6 +5,7 @@ type deleteReturn = {
 export default interface IBeerService<T>{
   create(beer: T): Promise<T>;
   readAll(): Promise<T[]>;
+  readById(id: string): Promise<T | null>;
   update(id: string, beer: Partial<T>): Promise<T | null>;
   delete(id: string): Promise<deleteReturn>
 }
