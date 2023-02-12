6cc5713f8cdfe4137065c8477f40e7a726ff44f0
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-12 10:49:57 -0300

    feat: ensure return 204 on success

--- src/main/routes/brewery-routes.ts
+++ src/main/routes/brewery-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeLoadBreweriesController } from '../factories/controllers/load-breweries-controller-factory'
 import { makeDeleteBreweryController } from '../factories/controllers/delete-brewery-controller-factory'
+import { makeAddBreweryController } from '../factories/controllers/add-brewery-controller'
 
 export default (router: Router): void => {
   router.get('/brewery', adaptRoute(makeLoadBreweriesController()))
   router.delete('/brewery/:id', adaptRoute(makeDeleteBreweryController()))
+  router.post('/brewery', adaptRoute(makeAddBreweryController()))
 }
