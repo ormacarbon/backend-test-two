637782fcf42e47432d362293c8cb85696f00178b
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-12 08:44:27 -0300

    rafactor: move sut creation for factory method

--- tests/data/usecases/db-add-brewery.spec.ts
+++ tests/data/usecases/db-add-brewery.spec.ts
@@ -2,10 +2,20 @@ import { DbAddBrewery } from '../../../src/data/usecases/db-add-brewery'
 import { mockAddBreweryParams } from '../../domain/mocks/mock-brewery'
 import { AddBreweryRepositorySpy } from '../mocks/mock-db-brewery'
 
+type SutTypes = {
+  sut: DbAddBrewery
+  addBreweryRepositorySpy: AddBreweryRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const addBreweryRepositorySpy = new AddBreweryRepositorySpy()
+  const sut = new DbAddBrewery(addBreweryRepositorySpy)
+  return { sut, addBreweryRepositorySpy }
+}
+
 describe('DbAddBrewery Usecase', () => {
   it('Should call AddBreweryRepository with correct values', async () => {
-    const addBreweryRepositorySpy = new AddBreweryRepositorySpy()
-    const sut = new DbAddBrewery(addBreweryRepositorySpy)
+    const { sut, addBreweryRepositorySpy } = makeSut()
     const params = mockAddBreweryParams()
     await sut.handle(params)
     expect(addBreweryRepositorySpy.params).toBe(params)
