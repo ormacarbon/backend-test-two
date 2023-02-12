7f73a951ffe60dbedd2abd67ce358f073ccd1f38
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-12 14:10:25 -0300

    feat: ensure return 500 if UpdateBrewery throws

--- src/presentation/controllers/update-brewery-controller.ts
+++ src/presentation/controllers/update-brewery-controller.ts
@@ -1,13 +1,17 @@
 import { UpdateBrewery, UpdateBreweryParams } from '../../domain/usecases/update-brewery'
 import { Controller } from '../abstract/controller'
 import { HttpResponse } from '../abstract/http'
-import { noContent } from '../helpers/http-helper'
+import { noContent, serverError } from '../helpers/http-helper'
 
 export class UpdateBreweryController implements Controller {
   constructor (private readonly udpateBrewery: UpdateBrewery) {}
   async handle (request: UpdateBreweryControllerRequest): Promise<HttpResponse> {
-    await this.udpateBrewery.handle(request)
-    return noContent()
+    try {
+      await this.udpateBrewery.handle(request)
+      return noContent()
+    } catch (error: any) {
+      return serverError(error)
+    }
   }
 }
 
