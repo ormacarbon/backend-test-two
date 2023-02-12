e040008bfccce648fdd3c9762dd25729c761296d
Hugo <huleonardo19@gmail.com>
2023-02-11 23:00:01 -0300

    fix(ErrorHandler): muda mensagem de retorno de erro em caso de status 500.

--- src/Middlewares/ErrorHandler.ts
+++ src/Middlewares/ErrorHandler.ts
@@ -10,7 +10,7 @@ class ErrorHandler {
   ) {
     console.log(error);
     if (error.statusCode) return res.status(error.statusCode).json({ message: error.message });
-    return res.status(500).json({ message: 'Something hapeend.' });
+    return res.status(500).json({ message: error.message });
   }
 }
 
