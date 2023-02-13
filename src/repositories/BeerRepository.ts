1a2362ff0392519d52446f4592c4190a75e60fff
Leonardo Santos <leonardo.santos574@hotmail.com>
2023-02-13 08:37:49 -0300

    add model beer, beer repository, and implementing find method

---
+++ src/repositories/BeerRepository.ts
@@ -0,0 +1,36 @@
+import { connect, disconnect } from "../config/database/database";
+import { Beer } from "../models/Beer";
+import { IBeerRepository } from "./IBeerRepository";
+
+
+class BeerRepository implements IBeerRepository{
+    insertOne({ abv, address, category, city, coordinates, country, description, ibu, name, state, website }: Beer): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+  
+    async find() {
+        try {
+
+            const database = await connect();
+            const beers = await database.collection("beers").find({}).toArray();
+
+            return beers;
+        } catch (err) {
+
+            console.log(err)
+            disconnect();
+            throw new Error("Something wrong happened");
+        }
+
+    }
+    findOne(id: string) {
+        throw new Error("Method not implemented.");
+    }
+    deleteOne(id: string) {
+        throw new Error("Method not implemented.");
+    }
+    updateOne(id: string, { abv, address, category, city, coordinates, country, description, ibu, name, state, website }: Beer) {
+        throw new Error("Method not implemented.");
+    }
+
+}
