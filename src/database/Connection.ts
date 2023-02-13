cb3c3ba4c11d77bb7a62912d56207c7e42f1191c
caiooliveirah09 <leelros@hotmail.com>
2023-02-13 16:40:30 -0300

    changed mongo db url

--- src/database/Connection.ts
+++ src/database/Connection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 const MONGO_DB_URL =
-  process.env.MONGO_DB_URL || "mongodb://mongodb:27017/MyEnterprise";
+  process.env.MONGO_DB_URL || "mongodb://localhost:27017/MyEnterprise";
 
 const connectToDatabase = (
   mongoDatabaseURL: string = MONGO_DB_URL
