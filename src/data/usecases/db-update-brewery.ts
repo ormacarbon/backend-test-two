fb458ede003fe3fe302dd0211375e3325fcfd694
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-13 15:18:00 -0300

    feat: ensure call LoadBreweryRepository with correct values

--- src/data/usecases/db-update-brewery.ts
+++ src/data/usecases/db-update-brewery.ts
@@ -1,9 +1,11 @@
 import { UpdateBrewery, UpdateBreweryParams } from '../../domain/usecases/update-brewery'
+import { LoadBreweryRepository } from '../protocols/db/load-brewery-repository'
 import { UpdateBreweryRepository } from '../protocols/db/update-brewery-repository'
 
 export class DbUpdateBrewery implements UpdateBrewery {
-  constructor (private readonly updateBreweryRepository: UpdateBreweryRepository) {}
+  constructor (private readonly updateBreweryRepository: UpdateBreweryRepository, private readonly loadBreweryRepository: LoadBreweryRepository) {}
   async handle (params: UpdateBreweryParams): Promise<void> {
+    await this.loadBreweryRepository.handle({ id: params.id })
     await this.updateBreweryRepository.handle(params)
   }
 }
