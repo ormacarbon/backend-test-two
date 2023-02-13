c14a25019ea16a43cfb1e12b6508f58abc2cfec7
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-13 15:18:19 -0300

    test: ensure call LoadBreweryRepository with correct values

--- tests/data/usecases/db-update-brewery.spec.ts
+++ tests/data/usecases/db-update-brewery.spec.ts
@@ -1,18 +1,21 @@
 import { DbUpdateBrewery } from '../../../src/data/usecases/db-update-brewery'
 import { mockUpdateBreweryParams } from '../../domain/mocks/mock-brewery'
-import { UpdateBreweryRepositorySpy } from '../mocks/mock-db-brewery'
+import { LoadBreweryRepositorySpy, UpdateBreweryRepositorySpy } from '../mocks/mock-db-brewery'
 
 type SutTypes = {
+  loadBreweryRepositorySpy: LoadBreweryRepositorySpy
   updateBrewerySpy: UpdateBreweryRepositorySpy
   sut: DbUpdateBrewery
 }
 
 const makeSut = (): SutTypes => {
+  const loadBreweryRepositorySpy = new LoadBreweryRepositorySpy()
   const updateBrewerySpy = new UpdateBreweryRepositorySpy()
-  const sut = new DbUpdateBrewery(updateBrewerySpy)
+  const sut = new DbUpdateBrewery(updateBrewerySpy, loadBreweryRepositorySpy)
   return {
     sut,
-    updateBrewerySpy
+    updateBrewerySpy,
+    loadBreweryRepositorySpy
   }
 }
 
@@ -22,5 +25,12 @@ describe('DbUpdateBrewery Usecase', () => {
     const params = mockUpdateBreweryParams()
     await sut.handle(params)
     expect(updateBrewerySpy.params).toBe(params)
+  })
+
+  it('Should call LoadBreweryRepository with correct values', async () => {
+    const { sut, loadBreweryRepositorySpy } = makeSut()
+    const params = mockUpdateBreweryParams()
+    await sut.handle(params)
+    expect(loadBreweryRepositorySpy.params).toEqual({ id: params.id })
   })
 })
