69e3cde6b81e22d3a4222b526651f1aad90ff02e
Rafael Coura <rafaelcoura1000@gmail.com>
2023-02-13 14:42:28 -0300

    feat: ensure return brewery on success

---
+++ src/infra/db/prisma/load-brewery-prisma-repository.ts
@@ -0,0 +1,10 @@
+import { LoadBreweryRepository, LoadBreweryRepositoryParams } from '../../../data/protocols/db/load-brewery-repository'
+import { BreweryEntity } from '../../../domain/entities/brewery'
+import { PrismaHelper } from './prisma-helper'
+
+export class LoadBreweryPrismaRepository implements LoadBreweryRepository {
+  async handle (params: LoadBreweryRepositoryParams): Promise<BreweryEntity> {
+    const brewery = await PrismaHelper.prisma.brewery.findUnique({ where: { id: params.id } })
+    return PrismaHelper.map(brewery)
+  }
+}
