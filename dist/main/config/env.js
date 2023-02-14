3fe6d996723f6e80b5f94a937a4e3f79e84058ed
Douglinhas <daguiaralcantara@gmail.com>
2023-02-14 18:17:11 -0300

    Trying again

--- dist/main/config/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: process.env.PORT || 5050,
-    mongo_url: process.env.MONGO_URL || "mongodb://localhost/beers"
-};
-//# sourceMappingURL=env.js.map
--- dist/main/middlewares/content-types/content-types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contentType = void 0;
-const contentType = (req, res, next) => {
-    res.type("json");
-    next();
-};
-exports.contentType = contentType;
-//# sourceMappingURL=content-types.js.map
